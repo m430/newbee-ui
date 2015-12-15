024fc3804d9c8c5190f33ceeb6a755dea0a32030
Andy Zheng <360475097@qq.com>
2015-12-15 23:33:10 +0800

    update the word component.

--- src/words/words.js
+++ src/words/words.js
@@ -2,79 +2,48 @@ angular.module('newbee.words', [])
 
 .directive('nbWord', NbWordDirective);
 
-// NbWordDirective.$inject = ['$scope'];
+function NbWordDirective($interval) {
 
-function NbWordDirective() {
-	
 	var letterStyles = ['letter-rotatex', 'letter-rotatey', 'letter-scale', 'letter-roll', 'letter-type'];
 	function getController(scope, ele, attrs) {
 		console.log(scope.words);
 	}
-	function getTemplate(iElement, iAttrs) {
-		var style = iAttrs.nbStyle || 'rotate';	// default style is rotate
-		var words = ['no', 'words', 'specified'];
+	function getTemplate($ele, $attrs) {
+		var style = $attrs.nbStyle || 'rotate';	// default style is rotate
 		var tpl = '<span class="nb-words">';
 		if (letterStyles.indexOf(style) != -1) {
-			for (var i = 0; i < words.length; i++) {
-				var word = '<b ' + (i == 0 ? 'class="nb-visible"' : '') + '>' + word2Letters(words[i]) + '</b>';
-				tpl += word;
-			}
+			tpl += '<b ng-repeat="word in words" ng-class="{\'nb-visible\':activeIndex == $index, \'nb-hidden\':activeIndex != $index}"><i ng-repeat="letter in word2Letters(word)"><em>{{letter}}</em></i></b>';
 		} else {
-			// for (var i = 0; i < words.length; i++) {
-			// 	var word = '<b ' + (i == 0 ? 'class="nb-visible"' : '') + '>' + words[i] + '</b>';
-			// 	tpl += word;
-			// }
 			tpl += '<b ng-repeat="word in words" ng-class="{\'nb-visible\':activeIndex == $index, \'nb-hidden\':activeIndex != $index}">{{word}}</b>';
 		}
 		tpl += '</span>';
 		return tpl;
-		// return '<div>Hello words {{dataWords}}</div>';
-
 	}
-	function getLink(scope, iElement, iAttrs, reCtrl) {
-		var _duration = parseInt(iAttrs.nbDuration, 10) || 2500;
+	function getLink($scope, $ele, $attrs, $ctrl) {
+		var _duration = parseInt($attrs.nbDuration, 10) || 2500;
 		var animationDuration = 1200;
 		var letterAnimateDelay = 80;
 		var _letterDuration = 800;
 
-		var style = iAttrs.nbStyle || 'rotate';
-		iElement.addClass((letterStyles.indexOf(style) != -1 ? 'nb-' : 'nb-words-') + style);
-		var $words = iElement.find('b');
-		scope.activeIndex = 0;
+		var style = $attrs.nbStyle || 'rotate';
+		$ele.addClass((letterStyles.indexOf(style) != -1 ? 'nb-' : 'nb-words-') + style);
 
-		var $animatorTimer;
-		function animator() {
-			if (scope.activeIndex >= scope.words.length) {
-				scope.activeIndex = 0;
-			}
-			scope.activeIndex++;
-			scope.$apply();
-
-			$animatorTimer = setTimeout(animator, _duration + animationDuration);
+		$scope.word2Letters = function (word) {
+			return word.split('');
 		}
+		$scope.activeIndex = 0;
+		var $animatorTimer = $interval(function() {
+			if ($scope.activeIndex >= $scope.words.length) {
+				$scope.activeIndex = 0;
+			}
+			$scope.activeIndex++;
+		}, _duration + animationDuration);
 
-		animator();
-		scope.$on('$destroy', function() {
-			clearTimeout($animatorTimer);
+		$scope.$on('$destroy', function() {
+			// $timeout.cancel($animatorTimer);
+			$interval.cancel($animatorTimer);
 		});
 	}
-	function word2Letters(word) {
-		var letters = '';
-		for (var i = 0; i < word.length; i++) {
-			letters += '<i><em>' + word[i] + '</em></i>';
-		}
-		return letters;
-	}
-	function getByIndex(arr, index) {
-		if (index < 0) {
-			return arr[arr.length - 1];
-		} else if (index>= arr.length) {
-			return arr[0];
-		} else {
-			return arr[index];
-		}
-	}
-
 	return {
 		restrict: 'E',
 		replace: true,
