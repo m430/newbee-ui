4db7057009221f0165f97ab3bc752e0724d5544d
Andy Zheng <azheng@objectivasoftware.com>
2015-12-28 15:38:55 +0800

    complete a custom select component.

--- src/select/select.js
+++ src/select/select.js
@@ -6,43 +6,35 @@ function NbSelectDirective() {
 	function getTemplate(iEle, iAttrs) {
 		var defaultOption = iAttrs.nbDefaultOption || 'Please set default option';
 		var theme = iAttrs.nbTheme || 'default';
-		var tpl = '<div class="selectholder">\
-						    <label>'+defaultOption+'</label>\
-						    <select name="projectstart" id="projectstart">\
-						    	<option on-finish-render="ngRepeatFinished" ng-repeat="option in nbOptions" value="{{$index}}">{{option.value}}</option>\
+		var tpl = '<div class="nb-select">\
+						    <select>\
+						    	<option value="0">' + defaultOption + '</option>\
+						    	<option on-finish-render="ngRepeatFinished" ng-repeat="option in nbOptions" value="{{$index + 1}}">{{option.value}}</option>\
 						    </select>\
+						    <span class="desc">' + defaultOption + '</span><span class="pulldown"></span><div class="dropdown"></div>\
 						  </div>';
 		return tpl;
 	}
 	function getLink(scope, iEle, iAttrs, ctrl) {
-		iEle.children().hide();
-		var desc = iEle.children('label').text();
-		iEle.append('<span class="desc">' + desc + '</span><span class="pulldown"></span><div class="selectdropdown"></div>');
+		iEle.children('select').hide();
 		scope.$on('ngRepeatFinished', function(e) {
 			iEle.children('select').children('option').each(function(index, el) {
 				if ($(this).attr('value') != '0') {
-					var $drop = $(this).parent().siblings('.selectdropdown');
+					var $drop = $(this).parent().siblings('.dropdown');
 					var name = $(this).text();
 					$drop.append('<span>'+name+'</span>');
 				}
 			});
 
 			iEle.click(function(event) {
-				if (iEle.hasClass('activeselectholder')) {
+				if (iEle.hasClass('nb-select-active')) {
 					// roll up roll up
-					iEle.children('.selectdropdown').slideUp(200);
-					iEle.removeClass('activeselectholder');
-					// change span back to selected option text
-					if(iEle.children('select').val() != '0') {
-						iEle.children('.desc').fadeOut(100, function(){
-							iEle.text(iEle.siblings("select").val());
-							iEle.fadeIn(100);
-						});
-					}
+					iEle.children('.dropdown').slideUp(200);
+					iEle.removeClass('nb-select-active');
 				} else {
 					// if there are any other open dropdowns, close 'em
-					$('.activeselectholder').each(function(){
-						$(this).children('.selectdropdown').slideUp(200);
+					$('.nb-select-active').each(function(){
+						$(this).children('.dropdown').slideUp(200);
 						// change span back to selected option text
 						if($(this).children('select').val() != '0') {
 							$(this).children('.desc').fadeOut(100, function(){
@@ -50,31 +42,21 @@ function NbSelectDirective() {
 								$(this).fadeIn(100);
 							});
 						}
-						$(this).removeClass('activeselectholder');
+						$(this).removeClass('nb-select-active');
 					});			
 					// roll down
-					iEle.children('.selectdropdown').slideDown(200);
-					iEle.addClass('activeselectholder');
-					// change span to show select box title while open
-					if(iEle.children('select').val() != '0') {
-						iEle.children('.desc').fadeOut(100, function(){
-							iEle.text(iEle.siblings("select").children("option[value=0]").text());
-							iEle.fadeIn(100);
-						});
-					}
+					iEle.children('.dropdown').slideDown(200);
+					iEle.addClass('nb-select-active');
 				}
 			});
 
 			// select dropdown click action
-			$('.selectholder .selectdropdown span').click(function(){
+			iEle.find('.dropdown span').click(function(event){
 				$(this).siblings().removeClass('active');
 				$(this).addClass('active');
 				var value = $(this).text();
 				$(this).parent().siblings('select').val(value);
-				$(this).parent().siblings('.desc').fadeOut(100, function(){
-					$(this).text(value);
-					$(this).fadeIn(100);
-				});
+				$(this).parent().siblings('.desc').text(value);
 			});
 		});
 	}
