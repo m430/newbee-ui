41ee5836011456ac229cff0ed9ecf925b415e7d0
Andy Zheng <azheng@objectivasoftware.com>
2015-12-29 13:58:03 +0800

    finished the input components.

--- src/input/input.js
+++ src/input/input.js
@@ -5,17 +5,24 @@ angular.module('newbee.input', [])
 function NbInputDirective() {
 	function getTemplate(iEle, iAttrs) {
 		var theme = iAttrs.nbTheme || 'haruki';
+		var id = iAttrs.nbId || '';
 		var label = iAttrs.nbLabel || 'Please input somthing..';
 		var tpl = '<span class="nb-input nb-input-' + theme + '">\
-					<input type="text" id="input-1">\
-					<label for="input-1">\
+					<input type="text" id="' + id + '">\
+					<label for="' + id + '" data-content="' + label + '">\
 						<span>' + label + '</span>\
 					</label>\
 				</span>';
 		return tpl;
 	}
 	function getLink(scope, iEle, iAttrs, ctrl) {
-
+		iEle.find('input').blur(function(event) {
+			if ($(this).val().trim()) {
+				$(this).addClass('nb-input-filled');
+			} else {
+				$(this).removeClass('nb-input-filled');
+			}
+		});
 	}
 	return {
 		restrict: 'E',
