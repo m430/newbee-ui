83936e71fb86e36f14c060e1757fa2a079e4ce20
Andy Zheng <azheng@objectivasoftware.com>
2015-12-30 16:30:22 +0800

    add a linear progress component.

--- src/progress/progress.js
+++ src/progress/progress.js
@@ -4,18 +4,23 @@ angular.module('newbee.progress', [])
 
 function NbProgressDirective() {
 	function getTemplate(iEle, iAttrs) {
-		var tpl = '<div class="nb-progress">\
+		var type = iAttrs.nbType || 'circle';
+		var tpls = {
+			circle: '<div class="nb-progress nb-progress-' + type + '">\
 								<div class="progress"><div class="progress-fill" style="transform: rotate({{percent * 3.6}}deg)"></div></div>\
 							  <div class="percents">\
 							    <span>{{percent + "%"}}</span>\
 							  </div>\
-							</div>';  
-		return tpl;
+							</div>',
+			linear: '<div class="nb-progress nb-progress-' + type + '"></div>'
+		}
+		return tpls[type];
 	}
 	function getLink(scope, iEle, iAttrs, ctrl) {
 		var percent = scope.percent || 0;
+		var type = iAttrs.nbType;
 		scope.$watch('percent', function(newVal, oldVal, scope) {
-			if (newVal > 50) {
+			if (newVal > 50 && type == 'circle') {
 				iEle.addClass('gt-50');
 			} else {
 				iEle.removeClass('gt-50');
